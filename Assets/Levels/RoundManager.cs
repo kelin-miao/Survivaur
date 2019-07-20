acade19cc676c3643282abef7faa93e6b5a1e37e
kelin-miao <41737136+kelin-miao@users.noreply.github.com>
2019-07-21 09:47:12 +1000

    Changed how camera works
    kept original code for camera commented in script
    
    camera is now more fluid and has a min and max variable

--- Assets/Levels/RoundManager.cs
+++ Assets/Levels/RoundManager.cs
@@ -31,17 +31,22 @@ public class RoundManager : MonoBehaviour
     public Transform dino2;
 
     //Center Vars
+
     private const float DISTANCE_MARGIN = 1.0f;
 
-    private Vector3 middlePoint;
-    private float distanceFromMiddlePoint;
-    private float distanceBetweenDinos;
-    private float cameraDistance;
+    //private Vector3 middlePoint;
+   // private float distanceFromMiddlePoint;
+    //private float distanceBetweenDinos;
+    //private float cameraDistance;
     private float aspectRatio;
-    private float fov;
-    private float tanFov;
+    //private float fov;
+    //private float tanFov;
 
     //Zoom Vars
+    private Vector3 velocity = Vector3.zero;
+    [SerializeField] public float smoothTime = 0.25f;
+    
+
     public float zoomSpeed = 3;
     public float targetOrtho;
     public float smoothSpeed = 1.0f;
@@ -62,13 +67,15 @@ public class RoundManager : MonoBehaviour
         //player2.tag = "Player 2";
         //Camera
         //Start Size
+
         //Camera.main.orthographicSize = 20;
+        //"The orthographicSize is half the size of the vertical viewing volume. The horizontal size of the viewing volume depends on the aspect ratio."
         //Centering
         aspectRatio = Screen.width / Screen.height;
-        tanFov = Mathf.Tan(Mathf.Deg2Rad * Camera.main.fieldOfView / 2.0f);
+        //tanFov = Mathf.Tan(Mathf.Deg2Rad * Camera.main.fieldOfView / 2.0f);
 
         //Zooming
-        targetOrtho = Camera.main.orthographicSize;
+        //targetOrtho = Camera.main.orthographicSize;
         dino1 = player1.GetComponent<Transform>();
         dino2 = player2.GetComponent<Transform>();
     }
@@ -102,40 +109,8 @@ public class RoundManager : MonoBehaviour
             Tie();
         }
 
-
-
-
-
-
-        //Camera
-
-        //Keep Cam Centered between dinos
-        // Position the camera in the center.
-        Vector3 newCameraPos = Camera.main.transform.position;
-        newCameraPos.x = middlePoint.x;
-        newCameraPos.y = middlePoint.y;
-        Camera.main.transform.position = newCameraPos;
-
-        // Find center point between Dinos.
-        Vector3 vectorBetweenDinos = dino2.position - dino1.position;
-        middlePoint = dino1.position + 0.5f * vectorBetweenDinos;
-
-        // Calculate the new distance.
-        distanceBetweenDinos = vectorBetweenDinos.magnitude;
-        cameraDistance = (distanceBetweenDinos / 2.0f / aspectRatio) / tanFov;
-
-        // Move camera to new position.
-        Vector3 dir = (Camera.main.transform.position - middlePoint).normalized;
-        Camera.main.transform.position = middlePoint + dir * (cameraDistance + DISTANCE_MARGIN);
-
-        // Zoom out as distance between increases
-        if (distanceBetweenDinos != 0.0f)
-        {
-            targetOrtho = distanceBetweenDinos * zoomSpeed;
-            targetOrtho = Mathf.Clamp(targetOrtho, minOrtho, maxOrtho);
-        }
-
-        Camera.main.orthographicSize = Mathf.MoveTowards(Camera.main.orthographicSize, targetOrtho, smoothSpeed * Time.deltaTime);
+        CentreCamera();
+        ResizeCamera();
     }
     void Player1Death()
     {
@@ -191,5 +166,60 @@ public class RoundManager : MonoBehaviour
     {
         Time.timeScale = 1.0f;
         SceneManager.LoadScene("Menu");
+    }
+
+    void CentreCamera()
+    {
+
+
+        //Midpoint position formula (((x1+x2)/2), ((y1+y2)/2))
+        Vector3 playersMidpoint = new Vector3((dino1.position.x + dino2.position.x)/2.0f, (dino1.position.y + dino2.position.y) / 2.0f, Camera.main.transform.position.z);
+
+
+
+        //Move Camera
+        Camera.main.transform.position = Vector3.SmoothDamp(Camera.main.transform.position, playersMidpoint, ref velocity, smoothTime);
+
+
+        /*
+        //Camera
+
+        //Keep Cam Centered between dinos
+
+        // Position the camera in the center.
+
+        Vector3 newCameraPos = Camera.main.transform.position;
+        newCameraPos.x = middlePoint.x;
+        newCameraPos.y = middlePoint.y;
+        Camera.main.transform.position = newCameraPos;
+
+        // Find center point between Dinos.
+        Vector3 vectorBetweenDinos = dino2.position - dino1.position;
+        middlePoint = dino1.position + 0.5f * vectorBetweenDinos;
+
+        // Calculate the new distance.
+        distanceBetweenDinos = vectorBetweenDinos.magnitude;
+        cameraDistance = (distanceBetweenDinos / 2.0f / aspectRatio) / tanFov;
+
+        // Move camera to new position.
+        Vector3 dir = (Camera.main.transform.position - middlePoint).normalized;
+        Camera.main.transform.position = middlePoint + dir * (cameraDistance + DISTANCE_MARGIN);
+
+        // Zoom out as distance between increases
+        if (distanceBetweenDinos != 0.0f)
+        {
+            targetOrtho = distanceBetweenDinos * zoomSpeed;
+            targetOrtho = Mathf.Clamp(targetOrtho, minOrtho, maxOrtho);
+        }
+
+        Camera.main.orthographicSize = Mathf.MoveTowards(Camera.main.orthographicSize, targetOrtho, smoothSpeed * Time.deltaTime);
+        */
+    }
+
+    void ResizeCamera()
+    {
+        //calculate x distance 
+        float xDistance = Mathf.Clamp(Mathf.Abs(dino1.position.x - dino2.position.x), 12.0f, 24.0f);
+        Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, xDistance/Screen.width * Screen.height, 0.05f);
     }
 }
