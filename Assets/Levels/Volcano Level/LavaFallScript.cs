4e00ee8ca30bf0e3a8e187ae6c54d1d57ce7be65
kelin-miao <41737136+kelin-miao@users.noreply.github.com>
2019-08-04 08:51:16 +1000

    Added falling lava
    added falling lava prefab
    added lava spawner game obj
    ashfall is now prewarmed

---
+++ Assets/Levels/Volcano Level/LavaFallScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaFallScript : MonoBehaviour
+{
+    [SerializeField] private float lavaFallDamage = 20.0f;
+    [SerializeField] private float lavaKnockBackForce = 20.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            coll.gameObject.GetComponent<DinoScript>().Health = coll.gameObject.GetComponent<DinoScript>().Health - lavaFallDamage;
+            coll.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, lavaKnockBackForce);
+        }
+
+        Destroy(gameObject,0.0f);
+    }
+}
---
+++ Assets/Levels/Volcano Level/LavaFallSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaFallSpawner : MonoBehaviour
+{
+    [SerializeField] private float spawnDistance = 12.0f;
+    [SerializeField] private float timeTillRespawn = 0.5f;
+    [SerializeField] private GameObject lavaPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RecursiveSpawn();
+    }
+
+    void RecursiveSpawn()
+    {
+        float RandDistance = Random.Range(0, spawnDistance);
+        Instantiate(lavaPrefab,new Vector3(transform.position.x + RandDistance,transform.position.y,transform.position.z), transform.localRotation);
+
+        Invoke("RecursiveSpawn", timeTillRespawn);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
