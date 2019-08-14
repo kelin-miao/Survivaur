d48c13f46c2bc612b3605886e6206dd6c68758bc
Anne Caves <52474396+ACaves@users.noreply.github.com>
2019-08-14 11:44:59 +1000

    IceLevelPolish

---
+++ Assets/Levels/Ice Age Level/Scripts/IcicleDamage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IcicleDamage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 10f);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            float Health = collision.gameObject.GetComponent<DinoScript>().Health;
+
+            Health -= 10f;
+        }
+
+
+        Destroy(gameObject);
+
+    }
+
+    //private void OnCollisionEnter(Collision collision)
+    //{
+
+
+    //}
+}
